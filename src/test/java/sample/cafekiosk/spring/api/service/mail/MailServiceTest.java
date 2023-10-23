2672561e9fd31ea48a8045cc03de29a7a7c19dcc
jeong-yeji <jeongyeji.mail@gmail.com>
2023-10-23 16:48:14 +0900

    test: Mock 객체 생성

---
+++ src/test/java/sample/cafekiosk/spring/api/service/mail/MailServiceTest.java
@@ -0,0 +1,39 @@
+package sample.cafekiosk.spring.api.service.mail;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import sample.cafekiosk.spring.client.mail.MailSendClient;
+import sample.cafekiosk.spring.domain.history.mail.MailSendHistory;
+import sample.cafekiosk.spring.domain.history.mail.MailSendHistoryRepository;
+
+class MailServiceTest {
+
+    @DisplayName("메일 전송 테스트")
+    @Test
+    void sendMail() {
+        // given
+        MailSendClient mailSendClient = mock(MailSendClient.class);
+        MailSendHistoryRepository mailSendHistoryRepository = mock(
+            MailSendHistoryRepository.class);
+        MailService mailService = new MailService(mailSendClient, mailSendHistoryRepository);
+
+        // stubbing
+        when(mailSendClient.sendEmail(anyString(), anyString(), anyString(), anyString()))
+            .thenReturn(true);
+
+        // when
+        boolean result = mailService.sendMail("", "", "", "");
+
+        // then
+        assertThat(result).isTrue();
+        verify(mailSendHistoryRepository, times(1)).save(any(MailSendHistory.class));
+    }
+}
