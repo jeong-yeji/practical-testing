e3079e6c33fec6a0161171932ad4f5379372361d
jeong-yeji <jeongyeji.mail@gmail.com>
2023-10-23 14:48:52 +0900

    feat: Presentation Layer

--- src/main/java/sample/cafekiosk/spring/api/controller/product/ProductController.java
+++ src/main/java/sample/cafekiosk/spring/api/controller/product/ProductController.java
@@ -1,12 +1,16 @@
 package sample.cafekiosk.spring.api.controller.product;
 
+import java.util.List;
+import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import sample.cafekiosk.spring.api.ApiResponse;
+import sample.cafekiosk.spring.api.controller.product.dto.request.ProductCreateRequest;
 import sample.cafekiosk.spring.api.service.product.ProductService;
 import sample.cafekiosk.spring.api.service.product.response.ProductResponse;
-
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -14,9 +18,15 @@ public class ProductController {
 
     private final ProductService productService;
 
-    @GetMapping("/api/v1/products/selling")
-    public List<ProductResponse> getSellingProducts() {
-        return productService.getSellingProducts();
+    @PostMapping("/api/v1/products/new")
+    public ApiResponse<ProductResponse> createProduct(
+        @Valid @RequestBody ProductCreateRequest request) {
+        return ApiResponse.ok(productService.createProduct(request.toServiceRequest()));
     }
-    
+
+    @GetMapping("/api/v1/products/selling")
+    public ApiResponse<List<ProductResponse>> getSellingProducts() {
+        return ApiResponse.ok(productService.getSellingProducts());
+    }
+
 }
--- src/main/java/sample/cafekiosk/spring/api/service/product/ProductService.java
+++ src/main/java/sample/cafekiosk/spring/api/service/product/ProductService.java
@@ -1,26 +1,57 @@
 package sample.cafekiosk.spring.api.service.product;
 
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import sample.cafekiosk.spring.api.service.product.request.ProductCreateServiceRequest;
 import sample.cafekiosk.spring.api.service.product.response.ProductResponse;
 import sample.cafekiosk.spring.domain.product.Product;
 import sample.cafekiosk.spring.domain.product.ProductRepository;
 import sample.cafekiosk.spring.domain.product.ProductSellingStatus;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
+/*
+ * CQRS (Command and Query Responsibility Segregation) - Command와 Query 분리하자!
+ * readOnly = true: 읽기 전용
+ * CRUD에서 CUD 동작 X / only Read
+ * JPA : CUD 스냅샷 저장, 변경 감지 X => 성능 향상
+ * */
+@Transactional(readOnly = true)
 @Service
 @RequiredArgsConstructor
 public class ProductService {
 
     private final ProductRepository productRepository;
 
+    @Transactional
+    public ProductResponse createProduct(ProductCreateServiceRequest request) {
+        String nextProductNumber = createNextProductNumber();
+
+        Product product = request.toEntity(nextProductNumber);
+        Product savedProduct = productRepository.save(product);
+
+        return ProductResponse.of(savedProduct);
+    }
+
     public List<ProductResponse> getSellingProducts() {
-        List<Product> productList = productRepository.findAllBySellingStatusIn(ProductSellingStatus.forDisplay());
+        List<Product> productList = productRepository.findAllBySellingStatusIn(
+            ProductSellingStatus.forDisplay());
 
         return productList.stream()
-                .map(ProductResponse::of)
-                .collect(Collectors.toList());
+            .map(ProductResponse::of)
+            .collect(Collectors.toList());
+    }
+
+    private String createNextProductNumber() {
+        String latestProductNumber = productRepository.findLatestProductNumber();
+        if (latestProductNumber == null) {
+            return "001";
+        }
+
+        int latestProductNumberInt = Integer.parseInt(latestProductNumber);
+        int nextProductNumberInt = latestProductNumberInt + 1;
+
+        return String.format("%03d", nextProductNumberInt);
     }
 }
