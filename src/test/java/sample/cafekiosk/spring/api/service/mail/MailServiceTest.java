892ff94f417f62ee5b0bf52a313babf74e26c5b9
jeong-yeji <jeongyeji.mail@gmail.com>
2023-10-23 17:01:37 +0900

    test: BDDMockito

--- src/test/java/sample/cafekiosk/spring/api/service/mail/MailServiceTest.java
+++ src/test/java/sample/cafekiosk/spring/api/service/mail/MailServiceTest.java
@@ -3,16 +3,15 @@ package sample.cafekiosk.spring.api.service.mail;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 import sample.cafekiosk.spring.client.mail.MailSendClient;
 import sample.cafekiosk.spring.domain.history.mail.MailSendHistory;
@@ -21,7 +20,7 @@ import sample.cafekiosk.spring.domain.history.mail.MailSendHistoryRepository;
 @ExtendWith(MockitoExtension.class)
 class MailServiceTest {
 
-    @Spy
+    @Mock
     private MailSendClient mailSendClient;
 
     @Mock
@@ -33,10 +32,10 @@ class MailServiceTest {
     @DisplayName("메일 전송 테스트")
     @Test
     void sendMail() {
-        // stubbing
-        doReturn(true)
-            .when(mailSendClient)
-            .sendEmail(anyString(), anyString(), anyString(), anyString());
+        // given (stubbing)
+        BDDMockito.given(
+                mailSendClient.sendEmail(anyString(), anyString(), anyString(), anyString()))
+            .willReturn(true);
 
         // when
         boolean result = mailService.sendMail("", "", "", "");
